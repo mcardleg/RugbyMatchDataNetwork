5214fcfca2505b0d453dffcbd65ae083d0baf241
Gwen McArdle <79484098+mcardleg@users.noreply.github.com>
2021-03-16 10:12:02 -0400

    player number sends, delays and automatic requests added

--- coach.c
+++ coach.c
@@ -5,9 +5,18 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
+
+void delay(int seconds)
+{
+    int milli_seconds = 1000 * seconds;
+    clock_t start_time = clock();
+
+    while (clock() < start_time + milli_seconds);
+}
 
 void coach_tell(int sockfd)
 {
@@ -23,16 +32,14 @@ void coach_tell(int sockfd)
 }
 
 
-void func(int sockfd)
+void func(int sockfd)           //send requests for data periodically
 {
     char buff[MAX];
     int n;
     for (;;) {
         bzero(buff, sizeof(buff));
-        printf("Enter the string : ");
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n')
-            ;
+        printf("Requesting data.\n");
+        strcpy(buff, "request");
         write(sockfd, buff, sizeof(buff));
         bzero(buff, sizeof(buff));
         read(sockfd, buff, sizeof(buff));
@@ -41,6 +48,7 @@ void func(int sockfd)
             printf("Client Exit...\n");
             break;
         }
+        delay(500);
     }
 }
 
--- player.c
+++ player.c
@@ -5,9 +5,18 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
+
+void delay(int seconds)
+{
+    int milli_seconds = 1000 * seconds;
+    clock_t start_time = clock();
+
+    while (clock() < start_time + milli_seconds);
+}
 
 void player_tell(int sockfd)
 {
@@ -35,7 +44,8 @@ void func(int sockfd)
         bzero(buff, sizeof(buff));
         read(sockfd, buff, sizeof(buff));
         printf("From Server: %s\n", buff);
-        if ((strncmp(buff, "exit", 4)) == 0) {
+        delay(500);                                           //delay 2 seconds
+        if ((strncmp(buff, "exit", 4)) == 0) {              //fix exit
             printf("Client Exit...\n");
             break;
         }
